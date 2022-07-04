6f3ff10e64c867f8f46e5e7005b75ebf678f1233
khansun <khan.mahmud@outlook.com>
2022-07-04 13:48:27 +0600

    T-primes sieve array

--- cpp/230B.cpp
+++ cpp/230B.cpp
@@ -1,33 +1,44 @@
 #include <stdio.h>
 #include <bits/stdc++.h>
 #include<iostream>
+
 using namespace std;
+
 
 int main()
 {
-    int n ,primary;
-    cin>>n;
-    int prime[n];
-   for(int i = 0; i<n; i++){
-        cin>>primary;
-            int total = 0;
-	for (int j = 1; j <= sqrt(primary); j++) {
-		if (primary % j == 0) {
-			if (primary / j == j)
-				total++;
 
-			else
-				total = total + 2;
-		}
-	}
-        prime[i] = total;
-    }
- for(int i=0; i<n;i++){
-        if(prime[i]==3){
-            cout<<"YES\n";
+   long long sieveLen = 1000000;
+
+   long long sievePtr[sieveLen] = {0};
+    for(int i=2; i<=sieveLen; i++)
+    {
+        if(sievePtr[i]==0)
+        {
+           for(int j=2; i*j<=sieveLen; j++)
+           {
+               sievePtr[i*j]=1;
+           }
         }
-        else
-            cout<<"NO\n";
-
     }
+    int n;
+    long long int num, rootNum;
+    cin>>n;
+
+    
+   for(int i = 0; i<n; i++){
+        cin>>num;
+        rootNum = sqrt(num);
+        if(num<=1){
+            cout<<"NO"<<endl;
+        }
+        else if(rootNum*rootNum==num && sievePtr[rootNum]==0){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    
+    }
+    return 0;
 }
