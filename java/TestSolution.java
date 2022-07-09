8b45976348adb71b675ad245981af687fed4b931
khansun <khan.mahmud@outlook.com>
2022-07-09 14:34:51 +0600

    pallindrome-easy-leetcode

---
+++ java/TestSolution.java
@@ -0,0 +1,9 @@
+
+class TestSolution {
+	public static void main(String[] args) {
+		PalindromeNumber palNumb = new PalindromeNumber();
+		int target = -121;
+		System.out.println(palNumb.isPalindrome(target));
+	}
+
+} 
