a6595b145b9e8dd780d07c81061ce61ebc219604
khansun <khan.mahmud@outlook.com>
2022-07-27 00:43:57 +0600

    CF:1672C

--- cpp/1672C.cpp
+++ cpp/1672C.cpp
@@ -1,34 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    //int  tests;
-    //cin >> tests;
-    int n;
-    for (int j = 0; j < 1; j++)
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int  tests;
+    cin >> tests;
+    while (tests--)
     {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
+
+        int n;
         cin >> n;
-        int eq=0;
-        int arr [n+1];
-        
-        for (int i = 1; i <= n; i++)
+        int start=0, end=0;
+        int arr [n];
+        for (int i = 0; i < n; i++)
         {
             cin >> arr[i];            
-            if(arr[i]==arr[i-1] && i>1){
-                    if(i!=2){
-                    eq++;
-                    arr[i]=-1;
-                    arr[i-1]=-1; 
-                    }
+        }
+        for (int i = 1; i < n; i++){
+            if(arr[i]==arr[i-1]){
+                start=i;
+                break;
             }
         }
-        if (n==6 && arr[1]==1 && arr[2]==1 && arr[3]==2 && arr[4]==-1 && arr[5]==-1 && arr[6]==4)
-            cout << eq+1 << endl;
-        else
-            cout << eq << endl;
-        
+        for (int i = start+1; i < n; i++){
+            if(arr[i]==arr[i-1]){
+                end=i;
+            }
+        }
+
+        if(end==start+1){
+            cout << "1" << endl;;
+        }
+        else if(end-start<=0)
+        {
+            cout << "0" << endl;;
+        }
+        else{
+            cout << end-start-1 << endl;
+        }      
 
     }
 
